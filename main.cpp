c624606eff190f77830899b938272c7cc8e3660f
Agata Les <hello@agatales.pl>
2020-10-28 09:48:31 +0100

    Add function Add fot int parameters

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
+int Add(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	std::cout<<"Hello Fun with GIT"<<std::endl;
 	
-	int z = 0;
-	int y = 0;
+	int z = 5;
+	int y = 8;
 
-	int ala = 0;
+	int ala = Add(y, z);
 	
 	return 0;
 }
