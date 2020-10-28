700f444a630f562c422323309ab5301b5ef556ff
Agata Les <hello@agatales.pl>
2020-10-28 10:09:31 +0100

    Add Mul function to multiply int

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
+int Mul(int a, int b)
+{
+	return a * b;
+}
+
 int main()
 {
 	std::cout<<"Hello Fun with GIT"<<std::endl;
 	
 	int z = 0;
-	int y = 0;
+	int y = 5;
+	
+	int ex = Mul(2, 8);
 	
 	return 0;
 }
